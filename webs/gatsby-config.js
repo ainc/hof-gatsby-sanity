fa11f43bac556795750ab246216cdea04211ee6a
ColinCarver10 <ccarver410@gmail.com>
2024-02-15 12:55:27 -0500

    Integrated sanity with Gatsby & added auto refresh

--- webs/gatsby-config.js
+++ webs/gatsby-config.js
@@ -1,6 +1,13 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV || 'production'}`
+})
+const token = process.env.SANITY_READ_TOKEN
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   siteMetadata: {
     title: `hof-gatsby-sanity`,
@@ -10,7 +17,10 @@ module.exports = {
     resolve: 'gatsby-source-sanity',
     options: {
       "projectId": "b1gnc6rj",
-      "dataset": "production"
+      "dataset": "production",
+      token,
+      watchMode: !isProd,
+      overlayDrafts: !isProd && token,
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
 };
--- webs/src/pages/index.js
+++ webs/src/pages/index.js
@@ -1,11 +1,42 @@
 import * as React from "react"
+import { graphql } from 'gatsby'
+import { GatsbyImage} from 'gatsby-plugin-image'
 
+const IndexPage = ({data}) => {
+  console.log(data.allSanityInductee.totalCount);
+  const allSanityInductee = (data.allSanityInductee.nodes);
 
-const IndexPage = () => {
   return (
-    <h1>HOF</h1>
+    <div>
+    {allSanityInductee.map((node, index) => (
+      <div>
+      <h1>{node.name}</h1>
+      <p>{node.company}</p>
+      <GatsbyImage image={node.profilePhoto.asset.gatsbyImageData} className='rounded-circle' alt={node.name}/>
+      <p>{node.bio}</p>
+      </div>
+    ))}
+    </div>
   )
 }
+
+export const query = graphql`
+  query MyQuery {
+    allSanityInductee {
+      nodes {
+        name
+        company
+        profilePhoto {
+          asset {
+            gatsbyImageData(height: 300, width: 300)
+          }
+        }
+        bio
+      }
+    }
+  }
+`;
+
 
 export default IndexPage
 
