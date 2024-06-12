dc3c5744101deccd49b7f4a80a89f510030e0fad
Layth Rahman <laythrahman11@gmail.com>
2024-06-12 12:35:44 -0400

    Added  book.js

---
+++ webs/src/pages/book.js
@@ -0,0 +1,89 @@
+import React, {useState} from "react"
+import { graphql } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+import InducteeCard from "../components/InducteeCard/InducteeCard";
+import '../styles/main.scss';
+import * as styles from './index.module.scss'
+import Layout from "../components/Layout/Layout";
+import Sponsors from "../components/Sponsors/Sponsors";
+import InducteeNav from "../components/InducteeNav/InducteeNav";
+import Title from "../components/Title/Title";
+import Body from "../components/Body/Body";
+import Button from "../components/Button/Button";
+
+const BookPage = ({ data }) => {
+
+  return (
+    <Layout>
+      <Container>
+        <Row>
+          <Col>
+            <Title className='mx-4 py-5'>Unbridled Series</Title>
+          </Col>
+        </Row>
+    </Container>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query IndexPageQuery {
+    allSanityInductee {
+      nodes {
+        inductee {
+          name
+          company
+          profilePhoto {
+            asset {
+              gatsbyImageData
+            }
+          }
+          year(formatString: "YYYY")
+          title
+        }
+        inductionCeremonyVideo
+        profileVideo
+        slug {
+          current
+        }
+      }
+    }
+    allSanityEmergingEntrepreneur {
+      nodes {
+        inductee {
+          name
+          company
+          title
+          year(formatString: "YYYY")
+          profilePhoto {
+            asset {
+              gatsbyImageData
+            }
+          }
+        }
+        linkedin
+      }
+    }
+    allSanityEvent {
+      nodes {
+        location {
+          address
+          city
+          venue
+          zip
+          contact {
+            phoneNumber
+          }
+        }
+        registrationLink
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`;
+
+
+export default BookPage;
+
+export const Head = () => <title>Book Page</title>
