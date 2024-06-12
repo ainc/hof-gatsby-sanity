a6e1a98d48c13a53f0b0645441b498512ea50bd3
Layth Rahman <laythrahman11@gmail.com>
2024-06-12 15:54:45 -0400

    Changing to an <img> tag

--- webs/src/pages/book.js
+++ webs/src/pages/book.js
@@ -20,7 +20,7 @@ const BookPage = ({ data }) => {
         <Row>
           <Col>
             <Title className='mx-4 py-5'>Unbridled Series</Title> 
-            <GatsbyImage image='../images/Logo_Square.png' alt="Logo Square"/>
+            <img src="../images/Logo_Square.png" style="border: 1px solid black; max-width: 100px; margin: auto; margin-bottom: 1em;"/>
           </Col>
         </Row>
         <Row>
