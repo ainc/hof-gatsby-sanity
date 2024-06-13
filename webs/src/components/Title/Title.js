0ca16dd457593e323346e21f5bfae4f147176517
Layth Rahman <laythrahman11@gmail.com>
2024-06-13 09:47:44 -0400

    imported the StaticImage plugin

--- webs/src/components/Title/Title.js
+++ webs/src/components/Title/Title.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './title.scss'
+import {  StaticImage } from 'gatsby-plugin-image'
 const Title = (props) => {
     return (
       <h2 {...props} className={`title ${props.className}`}>
