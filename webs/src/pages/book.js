b9860ebec60a8c72b67972e9928cdf1b575ef55e
Layth Rahman <laythrahman11@gmail.com>
2024-06-13 11:59:59 -0400

    trying using a script

--- webs/src/pages/book.js
+++ webs/src/pages/book.js
@@ -46,15 +46,16 @@ const BookPage = ({ data }) => {
                   alt="Unbridled Series Vol. 1 Cover"
                   src='../images/UBS-V1-Cover.jpg'
                 />
-                <form onsubmit={
-                  window.location.href = 'https://www.amazon.com/dp/1619616815/'
-                }>
-                  <button type="submit" className={styles.UBSv1Button}>
+                  <button type="submit" onclick='UBSv1Button()'className={styles.UBSv1Button}>
                     <b>
                       Purchase Here: Unbridled Spirit Volume 1
                     </b>
                   </button>
-                </form>
+                  <script>
+                    function UBSv1Button(){
+                      window.location.href="https://www.amazon.com/dp/1619616815/"
+                    }
+                  </script>
             </Col>
             <Col className={`${styles.UBSColumn} mx-4 py-5`}>
               <StaticImage className={styles.UBSv2Cover}
