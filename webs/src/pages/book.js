215760b882e7ccee061e0e87d82688f84f322495
Layth Rahman <laythrahman11@gmail.com>
2024-06-13 11:07:05 -0400

    added UBS-V2-Cover

--- webs/src/pages/book.js
+++ webs/src/pages/book.js
@@ -46,6 +46,10 @@ const BookPage = ({ data }) => {
                   alt="Unbridled Series Vol. 1 Cover"
                   src='../images/UBS-V1-Cover.jpg'
                 />
+                <StaticImage className={styles.UBSv1}
+                  alt="Unbridled Series Vol. 2 Cover"
+                  src='../images/UBS-V2-Cover.jpg'
+                />
                 <Body></Body>
             </Col>
         </Row>
