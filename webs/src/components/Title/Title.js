5285cbf2a84b5f1089bf623ba1e95bab03211ad7
Layth Rahman <laythrahman11@gmail.com>
2024-06-13 10:03:31 -0400

    Changed the title.scss to try and align right through margin

--- webs/src/components/Title/Title.js
+++ webs/src/components/Title/Title.js
@@ -6,16 +6,20 @@ const Title = (props) => {
     return (
       <h2 {...props} className={`title ${props.className}`}>
         {props.children}
+        <a href="http://fellowship.awesomeinc.org/">
         <StaticImage className='aincImage'
           alt="Awesome Inc Logo"
           src='../images/Logo_Square.png'
         />
+        </a>
+        <a href="/founders-series">
         <StaticImage className='founderImage'
           width={60}
           height={60}
           alt="Founders Logo"
           src='../images/founders_logo.png'
         />
+        </a>
       </h2>
     )
 }
