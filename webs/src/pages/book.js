a069429f90641b40f46b914ce74c50622a3877c3
Layth Rahman <laythrahman11@gmail.com>
2024-06-13 11:38:59 -0400

    changed back to an href and bold

--- webs/src/pages/book.js
+++ webs/src/pages/book.js
@@ -46,7 +46,9 @@ const BookPage = ({ data }) => {
                   alt="Unbridled Series Vol. 1 Cover"
                   src='../images/UBS-V1-Cover.jpg'
                 />
-                  <button onClick={"window.location.href = 'https://www.amazon.com/dp/1619616815/';"} class="submit-button">Purchase Here: Unbridled Spirit Volume 1</button>
+                <b>
+                  <a href='https://www.amazon.com/dp/1619616815/'>Purchase Here: Unbridled Spirit Volume 1</a>
+                </b>
             </Col>
             <Col className={`${styles.UBSColumn} mx-4 py-4`}>
               <StaticImage className={styles.UBSv2Cover}
