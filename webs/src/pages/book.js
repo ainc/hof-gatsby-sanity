0806b5f887de8157a1c0d6279afa839709428106
Layth Rahman <laythrahman11@gmail.com>
2024-06-13 11:48:36 -0400

    added form and button

--- webs/src/pages/book.js
+++ webs/src/pages/book.js
@@ -46,9 +46,15 @@ const BookPage = ({ data }) => {
                   alt="Unbridled Series Vol. 1 Cover"
                   src='../images/UBS-V1-Cover.jpg'
                 />
-                <b>
-                  <a class="btn" target="_blank" href='https://www.amazon.com/dp/1619616815/' className={styles.UBSv1Button}>Purchase Here: Unbridled Spirit Volume 1</a>
-                </b>
+                <form onSubmit={
+                  location.href = 'https://www.amazon.com/dp/1619616815/'
+                }>
+                  <button type="submit" className={styles.UBSv1Button}>
+                    <b>
+                      Purchase Here: Unbridled Spirit Volume 1
+                    </b>
+                  </button>
+                </form>
             </Col>
             <Col className={`${styles.UBSColumn} mx-4 py-5`}>
               <StaticImage className={styles.UBSv2Cover}
