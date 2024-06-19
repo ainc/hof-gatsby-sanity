02df1e1d3a8118ee1fec88539303cda5f7286ddf
Ethan Gamble <ethangamble37@outlook.com>
2024-06-19 12:47:04 -0400

    fix header

--- webs/src/components/Title/Title.js
+++ webs/src/components/Title/Title.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './title.scss'
-import {  StaticImage } from 'gatsby-plugin-image'
 const Title = (props) => {
     return (
       <h2 {...props} className={`title ${props.className}`}>
