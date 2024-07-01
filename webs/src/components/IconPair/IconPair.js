a40d5c895c95824da4d532c9495c568402b0e413
Ethan Gamble <ethangamble37@outlook.com>
2024-07-01 11:54:50 -0400

    small tweaks

---
+++ webs/src/components/IconPair/IconPair.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image";
+import { Container, Row, Col, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import InducteeCard from "../InducteeCard/InducteeCard";
+import Title from "../Title/Title";
+//import * as styles from './iconpair.module.scss';
+import FellowshipLogo from '../../images/Logo_Square.png'
+import FoundersLogo from '../../images/founders_logo.png'
+
+const IconPair = ( props ) => {
+    return(
+        <div>
+            <a href='https://awesomeinc.org/fellowship/' target="_blank">
+                <StaticImage placeholder='blurred' src='../../images/Logo_Square.png'  className='mx-1' style={{maxHeight: '75px', maxWidth: '50px'}} />
+            </a>
+            <a href='/founders-series'>
+                <StaticImage placeholder='blurred' src='../../images/founders_logo.png' className='mx-1'/>
+            </a>
+        </div>
+    )
+}
+
+export default IconPair;
--- webs/src/pages/media/index.js
+++ webs/src/pages/media/index.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import Layout from '../../components/Layout/Layout';
-import Sponsors from '../../components/Sponsors/sponsors';
+import Sponsors from '../../components/Sponsors/Sponsors';
 import { graphql } from 'gatsby';
 import { StaticImage, GatsbyImage } from 'gatsby-plugin-image';
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Title from '../../components/Title/Title';
 import * as styles from './media.module.scss'
-
+import IconPair from '../../components/IconPair/IconPair';
 
 
 const MediaPage = ({data}) => {
@@ -36,86 +36,74 @@ const MediaPage = ({data}) => {
     } 
     return (
         <Layout>
-          <Row >
-            <Col md={8} sm={12}>
-                <Title className={styles.podcastHeader}>
-                    <h2>Podcast</h2>
-                </Title>
-                <iframe className={styles.podcastVideo} src="https://www.youtube.com/embed/videoseries?list=PL_YvoQ-KM3YHtlmn9_E841lpegYDVlXOk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <Title className={styles.podcastHeader}>
-                    <h2>Book</h2>
-                </Title>
-                <iframe className={styles.podcastVideo} src="https://www.youtube.com/embed/uUbLe0U6Fdk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <section className={styles.mediaInfo}>
-                    <h3>Stories inside include:</h3>
-                    <ul>
-                        <li>What Jim Host (Host Communications) did after going from having $1,800,000 to being in debt $560,000
-                       (inflation adjusted) in 1967 when he was 28 years old. - page 50</li>
-                        <li>The investment Bill Gatton (Bill Gatton Motors) made when he was 8 years old - page 192</li>
-                        <li>What nine Harvard business professors told John Y. Brown Jr when he asked for advice about running KFC (hint: the conversation was short) - page 36</li>
-                    </ul>
-                    <h3>Check out samples from the Audio version of Unbridled Spirit:</h3>
-                    <ul>
-                        <li><a href="https://www.instagram.com/p/BuXfzSyhFsr/?utm_source=ig_web_button_share_sheet">Junior Bridgeman</a></li>
-                        <li><a href="https://www.instagram.com/p/BtzIZFAh6Ow/?utm_source=ig_web_button_share_sheet">Lee Todd</a></li>
-                        <li><a href="https://www.instagram.com/p/BuEqiFYBh-o/?utm_source=ig_web_button_share_sheet">Jim Host</a></li>
-                    </ul>
-                    <p>Pick up a copy of the audio book here:
+          <Container>
+
+            <Row >
+                <Col md={8} sm={12}>
+                    <Title className='py-5'>Podcast</Title>
+                    <iframe className={styles.podcastVideo} src="https://www.youtube.com/embed/videoseries?list=PL_YvoQ-KM3YHtlmn9_E841lpegYDVlXOk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <Title className='py-5'>Book</Title>
+                    <iframe className={styles.podcastVideo} src="https://www.youtube.com/embed/uUbLe0U6Fdk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <section className={styles.mediaInfo}>
+                        <h3>Stories inside include:</h3>
                         <ul>
-                            <li><a href="http://amzn.to/2yrO97K">Unbridled Spirit on Amazon</a></li>
-                            <li><a href="https://adbl.co/2zJeaxo">Unbridled Spirit on Audible (Sign in with your Amazon account too!)</a></li>
-                            <li><a href="https://itunes.apple.com/us/audiobook/unbridled-spirit-lessons-in-life-business-from-kentuckys/id1441679300">Unbridled Spirit on iTunes</a></li>
+                            <li>What Jim Host (Host Communications) did after going from having $1,800,000 to being in debt $560,000
+                        (inflation adjusted) in 1967 when he was 28 years old. - page 50</li>
+                            <li>The investment Bill Gatton (Bill Gatton Motors) made when he was 8 years old - page 192</li>
+                            <li>What nine Harvard business professors told John Y. Brown Jr when he asked for advice about running KFC (hint: the conversation was short) - page 36</li>
                         </ul>
-                    </p>
-                    <p>For inquiries about using the book for a class on business, entrepreneurship and/or Kentucky history, please email <a href="mailto:garrett.fahrbach@entrepreneurhof.com">garrett.fahrbach@entrepreneurhof.com</a></p>
-                </section>
-                <Title className={styles.podcastHeader}>
-                    Recent Press
-                </Title>
-                
-                <PressComp allPress={allPress} /> 
-            </Col>
-            <Col md={4} sm={12}>
-                <Title className={styles.recentHeader}>
-                <h2>Recent</h2>
-                <div className={styles.logos}>
-                  <StaticImage 
-                    className={styles.awesomeLogo}
-                    src='../../images/Logo_Square.png'
-                    alt='Awesome Inc Logo'
-                  />
-                  <StaticImage 
-                    className={styles.founderLogo}
-                    src='../../images/founders_logo.png'
-                    alt='Founder Series'
-                  />
-                  </div>
-                </Title>
+                        <h3>Check out samples from the Audio version of Unbridled Spirit:</h3>
+                        <ul>
+                            <li><a href="https://www.instagram.com/p/BuXfzSyhFsr/?utm_source=ig_web_button_share_sheet">Junior Bridgeman</a></li>
+                            <li><a href="https://www.instagram.com/p/BtzIZFAh6Ow/?utm_source=ig_web_button_share_sheet">Lee Todd</a></li>
+                            <li><a href="https://www.instagram.com/p/BuEqiFYBh-o/?utm_source=ig_web_button_share_sheet">Jim Host</a></li>
+                        </ul>
+                        <p>Pick up a copy of the audio book here:
+                            <ul>
+                                <li><a href="http://amzn.to/2yrO97K">Unbridled Spirit on Amazon</a></li>
+                                <li><a href="https://adbl.co/2zJeaxo">Unbridled Spirit on Audible (Sign in with your Amazon account too!)</a></li>
+                                <li><a href="https://itunes.apple.com/us/audiobook/unbridled-spirit-lessons-in-life-business-from-kentuckys/id1441679300">Unbridled Spirit on iTunes</a></li>
+                            </ul>
+                        </p>
+                        <p>For inquiries about using the book for a class on business, entrepreneurship and/or Kentucky history, please email <a href="mailto:garrett.fahrbach@entrepreneurhof.com">garrett.fahrbach@entrepreneurhof.com</a></p>
+                    </section>
+                    <Title className={styles.podcastHeader}>
+                        Recent Press
+                    </Title>
+                    
+                    <PressComp allPress={allPress} /> 
+                </Col>
+                <Col md={4} sm={12}>
+                <div className='d-flex justify-content-between mx-4 py-4 align-items-center' style={{borderBottom: '1px solid #bbb'}}>
+                    <Title  style={{borderBottom: 'none'}}>Recent</Title>
+                    <IconPair />
+                </div>            
                 <ul className={styles.ceremonyVideos}>
-                    {allCeremony.map(node => (
-                        <li key={node.name}>
-                            <p>{node.name}</p>
-                            <a className={styles.videoLink} href={node.videoLink} target="_blank" rel="shadowbox">
-                                <GatsbyImage
-                                    className={styles.thumbnail} 
-                                    image={node.thumbnail.asset.gatsbyImageData} 
-                                    alt={node.name} 
-                                    
-                                />
-                                <div className={styles.play}>
-                                    <StaticImage 
-                                        className={styles.iconPlayCircle}
-                                        src='../../images/founders_logo_white_smallest.png'
+                        {allCeremony.map(node => (
+                            <li key={node.name}>
+                                <p>{node.name}</p>
+                                <a className={styles.videoLink} href={node.videoLink} target="_blank" rel="shadowbox">
+                                    <GatsbyImage
+                                        className={styles.thumbnail} 
+                                        image={node.image.asset.gatsbyImageData} 
+                                        alt={node.name} 
+                                        
                                     />
-                                    <p className={styles.videoText}>{node.name}</p>
-                                </div>
-                            </a>
-                           </li>
-                    ))}
-                </ul>
-            </Col>
-          </Row>
-        <Sponsors />
+                                    <div className={styles.play}>
+                                        <StaticImage 
+                                            className={styles.iconPlayCircle}
+                                            src='../../images/founders_logo_white_smallest.png'
+                                        />
+                                        <p className={styles.videoText}>{node.name}</p>
+                                    </div>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </Col>
+            </Row>
+            <Sponsors />
+          </Container>
         </Layout>
     );
 }
@@ -131,7 +119,7 @@ export const query = graphql`
                 link
             }
         }
-        allSanityCeremonyVideo {
+        allSanityCeremonyVideo(sort: {name: DESC}) {
             nodes {
                 name
                 videoLink
