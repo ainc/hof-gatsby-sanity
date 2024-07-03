9c8259a2c4da2879a38d084a523b3a3a15c9a3b7
Ethan Gamble <ethangamble37@outlook.com>
2024-07-03 15:00:16 -0400

    small tweaks

--- webs/src/pages/nominate/index.js
+++ webs/src/pages/nominate/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-
 import Layout from '../../components/Layout/Layout';
-import Sponsors from '../../components/Sponsors/sponsors';
+import Sponsors from '../../components/Sponsors/Sponsors';
 import IconPair from '../../components/IconPair/IconPair';
 import Title from '../../components/Title/Title';
 import { Container, Row, Col, Button, Stack } from "react-bootstrap";
