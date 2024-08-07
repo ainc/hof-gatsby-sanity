1dadf650cc9b80330207428d134f409662f6cdf3
ClConr <clark.conrad@uky.edu>
2024-08-07 13:50:54 -0400

    midway progress

--- webs/src/components/InducteeNav/InducteeNav.js
+++ webs/src/components/InducteeNav/InducteeNav.js
@@ -1,23 +1,59 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Container, Nav, NavDropdown } from 'react-bootstrap';
+import Isotope from "isotope-layout"
 import InducteeCard from "../InducteeCard/InducteeCard";
 import Title from "../Title/Title";
 import * as styles from './inducteenav.module.scss';
 
-const InducteeNav = ( props ) => {
+const InducteeNav = (props) => {
+
   const data = props.data
   const [selectedYear, setSelectedYear] = useState(null); // Initialize state for selected year
   const [animation, setAnimation] = useState(null);
-
+  const navsRef = useRef(null);
+  const animationRef = useRef(null);
+   // const [isotope, setIsotope] = React.useState(null);
+  const [filterKey, setFilterKey] = React.useState('*');
+  const isotope = useRef(null);
   // Function to handle year selection - pass in state values as props because each instance of the component needs to handle its own state
   const handleYearClick = (year) => {
-    setSelectedYear(year); // Update state when a year is clicked
-    props.setSelectedYear(year);
-    if (data.node.inductee.year !== year) {
-      data.node.inductee.year.setAnimation(styles.leaving)
-    }
+    //setSelectedYear(year); // Update state when a year is clicked
+    //props.setSelectedYear(year);
+    handleFilter(year ? `year-${year}` : '*');
   };
   
+  
+
+  React.useEffect(() => {
+    try {
+    isotope.current = new Isotope('.inductee-list', {
+      itemSelector: '.filter-item',
+      layoutMode: 'fitRows',
+    })
+  } catch (error) {
+    console.error("Error initializing Isotope:", error);
+  }
+
+    // cleanup
+    return () => 
+      {
+        if (isotope.current) {
+          isotope.current.destroy();
+        }
+      }
+  }, [])
+
+  React.useEffect(() => {
+    if (isotope.current) {
+    filterKey === '*'
+      ? isotope.current.arrange({filter: `*`})
+      : isotope.current.arrange({filter: `${filterKey}`})
+    }
+  }, [isotope, filterKey])
+
+  const handleFilter = (key) => {setFilterKey(key);}
+
   // Filter inductees based on the selected year
   if(props.selectedYear !== "2020"){
     const filteredInductees = props.selectedYear ? data.filter(node => node.inductee.year === props.selectedYear) : data;
@@ -30,7 +66,7 @@ const InducteeNav = ( props ) => {
           </div>
           <div>
             {/*Nav for large screens and wider */}
-            <Nav as="ul" className={`${styles.navFilter} d-none d-lg-block`}>
+            <Nav as="ul" className={`${styles.navFilter} d-none d-lg-block`} ref={navsRef}>
               <Nav.Item as="li" onClick={() => handleYearClick(null)} tabIndex='-1'>All</Nav.Item>
               <Nav.Item as="li" onClick={() => handleYearClick("2023")} tabIndex='-1'>2023</Nav.Item>
               <Nav.Item as="li" onClick={() => handleYearClick("2022")} tabIndex='-1'>2022</Nav.Item>
@@ -50,49 +86,49 @@ const InducteeNav = ( props ) => {
             {/*Nav for medium screens and smaller */}
             <Nav as="ul" className={`d-lg-none`}>
               <NavDropdown  className={styles.titleStyle} title={selectedYear || "Select Year"} id="collapsable-nav-dropdown">
-                <NavDropdown.Item onClick={() => handleYearClick(null)}>
+                <NavDropdown.Item  onClick={() => handleYearClick(null)}>
                   All
                 </NavDropdown.Item>   
-                <NavDropdown.Item onClick={() => handleYearClick("2023")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2023")}>
                   2023
                 </NavDropdown.Item>    
-                <NavDropdown.Item onClick={() => handleYearClick("2022")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2022")}>
                   2022
                 </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => handleYearClick("2021")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2021")}>
                   2021
                 </NavDropdown.Item>  
-                <NavDropdown.Item onClick={() => handleYearClick("2020")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2020")}>
                   2020
                 </NavDropdown.Item> 
-                <NavDropdown.Item onClick={() => handleYearClick("2019")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2019")}>
                   2019
                 </NavDropdown.Item>   
-                <NavDropdown.Item onClick={() => handleYearClick("2018")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2018")}>
                   2018
                 </NavDropdown.Item>   
-                <NavDropdown.Item onClick={() => handleYearClick("2017")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2017")}>
                   2017
                 </NavDropdown.Item>  
-                <NavDropdown.Item onClick={() => handleYearClick("2016")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2016")}>
                   2016
                 </NavDropdown.Item>   
-                <NavDropdown.Item onClick={() => handleYearClick("2015")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2015")}>
                   2015
                 </NavDropdown.Item>       
-                <NavDropdown.Item onClick={() => handleYearClick("2014")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2014")}>
                   2014
                 </NavDropdown.Item>  
-                <NavDropdown.Item onClick={() => handleYearClick("2013")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2013")}>
                   2013
                 </NavDropdown.Item>  
-                <NavDropdown.Item onClick={() => handleYearClick("2012")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2012")}>
                   2012
                 </NavDropdown.Item>  
-                <NavDropdown.Item onClick={() => handleYearClick("2011")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2011")}>
                   2011
                 </NavDropdown.Item>  
-                <NavDropdown.Item onClick={() => handleYearClick("2010")}>
+                <NavDropdown.Item  onClick={() => handleYearClick("2010")}>
                   2010
                 </NavDropdown.Item>  
               {/* Add more years here */}
@@ -101,11 +137,20 @@ const InducteeNav = ( props ) => {
           </div>
         </div>
         <div className='py-3 d-flex justify-content-center'>
-          <ul className={`${styles.inducteesList}`}>
+          <ul className={`inductee-list ${styles.inducteesList}`}>
+            
             {filteredInductees.map((node, index) => (
-              <li key={index}>
-                <InducteeCard img={node.inductee.profilePhoto.asset.gatsbyImageData} name={node.inductee.name} company={node.inductee.company} link={props.title === "Inductees" ? node.slug.current : node.linkedin} className={animation}/>
-              </li>
+                <li key={index}>
+                  <InducteeCard 
+                    img={node.inductee.profilePhoto.asset.gatsbyImageData} 
+                    name={node.inductee.name} 
+                    company={node.inductee.company} 
+                    link={props.title === "Inductees" ? node.slug.current : node.linkedin}
+                    year={node.inductee.year}
+                    className={`filter-item year-${node.inductee.year}`}
+                    //styles={{animationDuration: '3000ms'}}
+                  ></InducteeCard>
+                </li>
             ))}
           </ul>
         </div>
