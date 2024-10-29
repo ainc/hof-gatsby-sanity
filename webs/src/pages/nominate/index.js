b4fbeb08989761ebbb37d2ec88468780142f8416
nwils000 <nwils000@gmail.com>
2024-10-29 18:53:43 -0400

    only sanity changes

--- webs/src/pages/nominate/index.js
+++ webs/src/pages/nominate/index.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import * as React from 'react';
 import Layout from '../../components/Layout/Layout';
 import Sponsors from '../../components/Sponsors/Sponsors';
 import IconPair from '../../components/IconPair/IconPair';
 import Title from '../../components/Title/Title';
 import { Container, Button, Stack } from 'react-bootstrap';
 import * as styles from './nominate.module.scss';
-import { format } from 'date-fns';
 
 const NominatePage = () => {
-  // Do a QraphQL query
-  //   const data =
-  const buttons = data.allSanityNominationFormButton.nodes;
-
   return (
     <Layout>
       <Container>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <Title className={styles.header}>
           <h2>Nominate</h2>
           <IconPair className={styles.icons} />
         </Title>
         <section style={{ color: 'rgb(102,102,102)' }}>
           <p>
-            <b>Note:</b> Nominations for the 2024 class and beyond are open.{' '}
+            <b>Note: </b>Nominations for the 2024 class and beyond is open.{' '}
             <b>Deadline: May 3, 2024</b>
           </p>
           <h2>Selection Criteria</h2>
@@ -36,25 +33,52 @@ const NominatePage = () => {
           </ul>
         </section>
         <Stack gap={3} className={styles.buttonContainer} lg={12}>
-          {buttons.map((button) => (
-            <Button
-              key={button._id}
-              className={styles.button}
-              href={button.url}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {button.text} (click here, deadline:{' '}
-              {format(new Date(button.deadline), 'MM/dd/yy')})
-            </Button>
-          ))}
+          <Button
+            className={styles.button}
+            href="https://forms.zohopublic.com/virtualoffice9155/form/NominationKentuckyEntrepreneurHallofFame/formperma/RbkleuPk2I_uJqvvP0aAi2DrXVrfwso-QKVOWR6h_EI"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Hall of Fame Nominations (click here, deadline: 5/3/24)
+          </Button>
+          <Button
+            className={styles.button}
+            href="https://forms.zohopublic.com/virtualoffice9155/form/EmergingEntrepreneurNominationKentuckyEntrepreneur/formperma/WlfEONIkD_KdXHI5lfj9kXvZ8SUBgQTaC9bIf9AnGXk"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Emerging Entrepreneur Nominations (click here, deadline: 5/3/24)
+          </Button>
+          <Button
+            className={styles.button}
+            href="https://zfrmz.com/HiHnbru8T7pFNW1rTFpw"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mentor of the Year Award (click here, deadline: 5/3/24)
+          </Button>
+          <Button
+            className={styles.button}
+            href="https://zfrmz.com/FcyrqHK54OYvk507ovTQ"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Investor of the Year (click here, deadline: 5/3/24)
+          </Button>
+          <Button
+            className={styles.button}
+            href="https://zfrmz.com/PcjCQ9fgz9mjukf8hpv3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            President/Executive Nomination (click here, deadline: 5/3/24)
+          </Button>
         </Stack>
         <Sponsors />
       </Container>
     </Layout>
   );
 };
-
 export default NominatePage;
 
 export const Head = () => <title>Nominate</title>;
